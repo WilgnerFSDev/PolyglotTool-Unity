ebadd15434f1f40fb2b56130c5b1776a42a9ec3a
WilgnerFSDev <wilgner.fabio@gmail.com>
2018-10-18 23:31:14 -0300

    Polyglot Window and can be opened by MenuItem(Wilgner Studio/Polyglot Tool

---
+++ Assets/Wilgner Studio/WS_PolyglotTool/Editor/PolyglotWindow.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class PolyglotWindow : EditorWindow {
+
+    [MenuItem("Wilgner Studio/Polyglot Tool")]
+    public static void ShowWindow()
+    {
+        GetWindow<PolyglotWindow>("Polyglot Tool");
+    }
+
+    private void OnGUI()
+    {
+        
+    }
+}
